8fa4352cc466c0a64f0e405d28552decc901f8b1
rolah <nick1@freemail.hu>
2015-01-10 11:07:11 +0100

    JWPlayer add new files in setp -> datamigration

---
+++ Orchard.Web/Modules/JWPlayer/Models/JWPlayerPart.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Orchard.ContentManagement;
+
+namespace JWPlayer.Models
+{
+    public class JWPlayerPart : ContentPart<JWPlayerPartRecord>
+    {
+        [Required]
+        public string PlayerSource
+        {
+            get { return Record.PlayerSource; }
+            set { Record.PlayerSource = value; }
+        }
+        [Required]
+        public int Height
+        {
+            get { return Record.Height; }
+            set { Record.Height = value; }
+        }
+        [Required]
+        public int Width
+        {
+            get { return Record.Width; }
+            set { Record.Width = value; }
+        }
+        [Required]
+        public string MediaFile
+        {
+            get { return Record.MediaFile; }
+            set { Record.MediaFile = value; }
+        }
+        public bool AutoStart
+        {
+            get { return Record.AutoStart; }
+            set { Record.AutoStart = value; }
+        }
+        public bool Repeat
+        {
+            get { return Record.Repeat; }
+            set { Record.Repeat = value; }
+        }
+    }
+}

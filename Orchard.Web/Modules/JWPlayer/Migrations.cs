d14b11339e6fe7800a8ad87488564980ae868e5b
rolah <nick1@freemail.hu>
2015-01-10 11:17:53 +0100

    Add all JWPlayer module files before feature enabled

--- Orchard.Web/Modules/JWPlayer/Migrations.cs
+++ Orchard.Web/Modules/JWPlayer/Migrations.cs
@@ -11,9 +11,29 @@ namespace JWPlayer {
     public class Migrations : DataMigrationImpl {
 
         public int Create() {
-
+			// Creating table JWPlayerPartRecord
+			SchemaBuilder.CreateTable("JWPlayerPartRecord", table => table
+				.ContentPartRecord()
+				.Column("PlayerSource", DbType.String)
+				.Column("Height", DbType.Int32)
+				.Column("Width", DbType.Int32)
+				.Column("MediaFile", DbType.String)
+				.Column("AutoStart", DbType.Boolean)
+				.Column("Repeat", DbType.Boolean)
+			);
 
             return 1;
+        }
+
+        public int UpdateFrom1()
+        {
+            ContentDefinitionManager.AlterTypeDefinition(
+            "JWPlayerWidget", cfg => cfg
+            .WithPart("JWPlayerPart")
+            .WithPart("WidgetPart")
+            .WithPart("CommonPart")
+            .WithSetting("Stereotype", "Widget"));
+            return 2;
         }
     }
 }
